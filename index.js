a4a3ac74048afab7eee9050ae77a8127dd6e9705
Low Saetern <saetern.low123@yahoo.com>
2023-01-19 16:21:55 -0700

    local storage event listener for newform

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ window.addEventListener('load', () => {
 	nameInput.addEventListener('change', (e) => {
 		localStorage.setItem('username', e.target.value);
 	})
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+})
 })
