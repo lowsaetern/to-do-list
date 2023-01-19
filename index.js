51b34590e68f30813191b5e5233bfe4d4ae369e2
Low Saetern <saetern.low123@yahoo.com>
2023-01-19 16:20:34 -0700

    localstorage on load

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-:root {
-	--primary: #EA40A4;
-	--business: #3a82ee;
-	--personal: var(--primary);
-	--light: #EEE;
-	--grey: #888;
-	--dark: #313154;
-	--danger: #ff5b57;
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
 
-	--shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+	const username = localStorage.getItem('username') || '';
 
-	--business-glow: 0px 0px 4px rgba(58, 130, 238, 0.75);
-	--personal-glow: 0px 0px 4px rgba(234, 64, 164, 0.75);
-}
+	nameInput.value = username;
+
+	nameInput.addEventListener('change', (e) => {
+		localStorage.setItem('username', e.target.value);
+	})
+})
